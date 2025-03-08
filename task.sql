1f4c61632e4bcb4ca2b63d4d74edf58f665ab73d
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2025-03-09 00:03:53 +0200

    finish

--- task.sql
+++ task.sql
@@ -1,4 +1,5 @@
 CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB.Countries TO 'webappuser'@'%';
+
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL ON ShopDB.Countries TO 'deploymentuser'@'%';
